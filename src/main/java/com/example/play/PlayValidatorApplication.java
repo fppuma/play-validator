0ede05dd0e6682e5affead98c96049b257b3ed43
Francisco Puma <fppuma@gmail.com>
2025-04-12 22:19:59 -0500

    initial commit

---
+++ src/main/java/com/example/play/PlayValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayValidatorApplication.class, args);
+	}
+
+}
